5c730298e9a3feb54e0b0be75b4c7dade1957f17
Luke Welna <lwelna@sofi.org>
2016-02-29 16:46:03 -0700

    convert tabs to spaces

--- GradeStep2.sh
+++ GradeStep2.sh
@@ -3,12 +3,12 @@
 INPUTS=Step2/inputs/*
 mkdir Step2/usertest
 for i in $INPUTS
-	do
-		filename=${i%.*}
-		name=${filename##*/}
-		echo "Testing input file $i"
-		output="${name}Test.out"
-		outtest="${name}.out"
-		./Micro $i > Step2/usertest/$output
-		diff -b -s Step2/usertest/$output Step2/outputs/$outtest
-	done
+    do
+        filename=${i%.*}
+        name=${filename##*/}
+        echo "Testing input file $i"
+        output="${name}Test.out"
+        outtest="${name}.out"
+        ./Micro $i > Step2/usertest/$output
+        diff -b -s Step2/usertest/$output Step2/outputs/$outtest
+    done
