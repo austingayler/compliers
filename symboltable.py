ef11491af0a567dc341b2c9e189afc46547943bc
AJ Gayler <austingayler@gmail.com>
2016-03-22 16:28:35 -0600

    add step 3 inputs

---
+++ symboltable.py
@@ -0,0 +1,30 @@
+class Symbol:
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+
+class SymbolTable(object):
+
+    def __init__(self, parent, name): # parent scope and symbol table name
+        self.symbols = {}
+        self.name = name
+        self.parent = parent
+
+    def put(self, symbol): # put variable symbol or fundef under <name> entry
+        if self.symbols.__contains__(symbol.name):
+            return False
+        else:
+            self.symbols[symbol.name]= symbol
+            return True
+
+    def get(self, name): # get variable symbol or fundef from <name> entry
+        if self.symbols.__contains__(name):
+            return self.symbols[name]
+        elif self.parent:
+            return self.parent.get(name)
+        else:
+            return None
+
+
+    def getParentScope(self):
+        return self.parent
