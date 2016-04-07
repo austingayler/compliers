e40c56234c841792fcac1ad0192cce6fbd59a739
Luke Welna <nitro.luke@gmail.com>
2016-04-07 11:49:03 -0600

    remove global ir_node_list

--- ir_to_tiny.py
+++ ir_to_tiny.py
@@ -4,15 +4,15 @@ import Node
 import Stack
 
 
-ir_node_list = []
-
-
 def read_file(file_name):
+    ir_node_list = []
     with open(file_name) as f:
         for instructions in f:
             split = instructions.split()
-            build_ir_node(split)
-
+            node = build_ir_node(split)
+            if node is not None:
+                ir_node_list.append(node)
+    return ir_node_list
 
 def build_ir_node(instructions_list):
     if "LABEL" in instructions_list[0] or "LINK" in instructions_list[0]:
@@ -35,8 +35,7 @@ def build_ir_node(instructions_list):
         except IndexError:
             result = ""
 
-        node = Node.IRNode(op_code, op1, op2, result)
-        ir_node_list.append(node)
+        return Node.IRNode(op_code, op1, op2, result)
 
 
 def transpile(ir_node_list):
@@ -77,7 +76,7 @@ def transpile(ir_node_list):
     return node_list
 
 
-def new_op(op1, op2="", op3 = ""):
+def new_op(op1, op2="", op3=""):
     # Strip "$T" from ir_node op codes and replace with "r". also decrement op values (may not be necessary).
     new_op1, new_op2, new_op3 = op1, op2, op3
     if "$T" in op1:
@@ -92,10 +91,7 @@ def new_op(op1, op2="", op3 = ""):
     return new_op1, new_op2, new_op3
 
 
-input = sys.argv[1]
-read_file(input)
-
-little_node_list = transpile(ir_node_list)
+little_node_list = transpile(read_file(sys.argv[1]))
 
 for node in little_node_list:
     print(node.op_code, node.op1, node.op2)
