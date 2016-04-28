a30a69a0e5419c688a1eade28063e5f1af79f2bc
Luke Welna <nitro.luke@gmail.com>
2016-04-28 11:03:13 -0600

    Improve parse tree. Fix errors with not seeing literals after assignemnts and other things.

--- parser.py
+++ parser.py
@@ -28,13 +28,13 @@ list_var_decl = False
 ## PROGRAM
 def p_program(p):
     'program : PROGRAM id BEGIN pgm_body END'
-    p[0] = (p[1], p[2], p[3], p[4], p[5])
+    p[0] = ("PROGRAM", p[1], p[2], p[3], p[4], p[5])
     end_scope()
 
 
 def p_id(p):
     'id : IDENTIFIER'
-    p[0] = p[1]
+    p[0] = ("IDENT", p[1])
     # print("Encountered ", p[1].strip(), "Scope stax name =", scope_stack.peek().name)
 
     if scope_stack.peek().name == "FUNC":
@@ -53,7 +53,7 @@ def p_id(p):
 
 def p_pgm_body(p):
     'pgm_body : pgm_body_var_decl_aux decl func_declarations'
-    p[0] = (p[1], p[2], p[3])
+    p[0] = ("PGRM_BODY", p[2], p[3])
 
 
 def p_pgm_body_var_decl_aux(p):
@@ -67,9 +67,9 @@ def p_decl(p):
     | var_decl decl
     | empty"""
     if len(p) == 3:
-        p[0] = (p[1], p[2])
+        p[0] = ("DECL", p[1], p[2])
 
-    if len(p) == 2:
+    elif len(p) == 2:
         global list_var_decl
         list_var_decl = False
 
@@ -77,7 +77,7 @@ def p_decl(p):
 ## GLOBAL STRING DECLARATIONS
 def p_string_decl(p):
     'string_decl : STRING id ASSIGN str SEMI'
-    p[0] = (p[1], p[2], p[3], p[4],p[5])
+    p[0] = ("STRING_DECL", p[1], p[2], p[3], p[4],p[5])
 
 
 def p_str(p):
@@ -90,7 +90,7 @@ def p_str(p):
 ## Variable Declaration
 def p_var_decl(p):
     'var_decl : var_type id_list SEMI'
-    p[0] = (p[1], p[2], p[3])
+    p[0] = ("VAR_DECL", p[1], p[2], p[3])
     global cur_sym_type
     cur_sym_type = None
 
@@ -98,7 +98,7 @@ def p_var_decl(p):
 def p_var_type(p):
     """var_type : FLOAT
     | INT"""
-    p[0] = p[1]
+    p[0] = ("VAR_TYPE", p[1])
     global cur_sym_type
     cur_sym_type = p[1].strip()
 
@@ -106,13 +106,13 @@ def p_var_type(p):
 def p_any_type(p):
     """any_type : var_type
     | VOID"""
-    p[0] = p[1]
+    p[0] = ("ANY_TYPE", p[1])
     new_func_scope()
 
 
 def p_id_list(p):
     'id_list : id id_tail'
-    p[0] = (p[1], p[2])
+    p[0] = ("ID_LIST", p[1], p[2])
     global cur_sym_type
     global id_list_symbols
     if list_var_decl:
@@ -126,7 +126,7 @@ def p_id_tail(p):
     """id_tail : COMMA id id_tail
     | empty"""
     if len(p) == 4:
-        p[0] = (p[1], p[2], p[3])
+        p[0] = ("ID_TAIL", p[1], p[2], p[3])
 
     if list_var_decl:
         global id_list_symbols
@@ -138,12 +138,12 @@ def p_param_decl_list(p):
     """param_decl_list : param_decl param_decl_tail
     | empty"""
     if len(p) == 3:
-        p[0] = (p[1], p[2])
+        p[0] = ("PARAM_DECL_LIST", p[1], p[2])
 
 
 def p_param_decl(p):
     'param_decl : var_type id'
-    p[0] = (p[1], p[2])
+    p[0] = ("PARAM_DECL", p[1], p[2])
     global cur_sym_type
     sym = Node.Symbol(id_stack.pop(), None, cur_sym_type)
     scope_stack.peek().add_symbol(sym)
@@ -153,25 +153,25 @@ def p_param_decl_tail(p):
     """param_decl_tail : COMMA param_decl param_decl_tail
     | empty"""
     if len(p) == 4:
-        p[0] = (p[1], p[2], p[3])
+        p[0] = ("PARAM_DECL_TAIL", p[1], p[2], p[3])
 
 ## Function Declarations
 def p_func_declarations(p):
     """func_declarations : func_decl func_declarations
     | empty"""
     if len(p) == 3:
-        p[0] = (p[1], p[2])
+        p[0] = ("FUNC_DECLARATIONS", p[1], p[2])
 
 
 def p_func_decl(p):
     """func_decl : FUNCTION any_type id LPAREN param_decl_list RPAREN BEGIN func_body END"""
-    p[0] = (p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
+    p[0] = ("FUNC_DECL", p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9])
     end_scope()
 
 
 def p_func_body(p):
     'func_body : list_var_decl_part decl stmt_list'
-    p[0] = (p[2], p[3])
+    p[0] = ("FUNC_BODY", p[2], p[3])
 
 
 def p_list_var_decl_part(p):
@@ -185,14 +185,14 @@ def p_stmt_list(p):
     """stmt_list : stmt stmt_list
     | empty"""
     if len(p) == 3:
-        p[0] = (p[1], p[2])
+        p[0] = ("STMT_LIST", p[1], p[2])
 
 
 def p_stmt(p):
     """stmt : base_stmt
     | if_stmt
     | while_stmt"""
-    p[0] = (p[1])
+    p[0] = ("STMT", p[1])
 
 
 def p_base_stmt(p):
@@ -200,83 +200,84 @@ def p_base_stmt(p):
     | read_stmt
     | write_stmt
     | return_stmt"""
-    p[0] = (p[1])
+    p[0] = ("BASE_STMT", p[1])
 
 
 ## Basic Statements
 def p_assign_stmt(p):
     """assign_stmt : assign_expr SEMI"""
-    p[0] = (p[1], p[2])
+    p[0] = ("ASSIGN_STMT", p[1], p[2])
 
 
 def p_assign_expr(p):
     """assign_expr : id ASSIGN expr"""
     #FIXME?
-    p[0] = (p[3], p[1], p[2])
+    p[0] = ("ASSIGN_EXPR", p[1], p[2], p[3])
 
 
 def p_read_stmt(p):
     """read_stmt : READ LPAREN id_list RPAREN SEMI"""
-    p[0] = (p[1], p[2], [3], p[4], p[5])
+    p[0] = ("READ_STMT", p[1], p[2], p[3], p[4], p[5])
 
 
 def p_write_stmt(p):
     """write_stmt : WRITE LPAREN id_list RPAREN SEMI"""
-    p[0] = (p[1], p[2], [3], p[4], p[5])
+    p[0] = ("WRITE_STMT", p[1], p[2], p[3], p[4], p[5])
 
 
 def p_return_stmt(p):
     """return_stmt : RETURN expr SEMI"""
-    p[0] = (p[1], p[2], [3])
+    p[0] = ("RETURN_STMT", p[1], p[2], p[3])
 
 
 ## Expressions
 def p_expr(p):
     """expr : expr_prefix factor"""
-    p[0] = (p[1], p[2])
+    p[0] = ("EXPR", p[1], p[2])
 
 
 def p_expr_prefix(p):
     """expr_prefix : expr_prefix factor addop
     | empty"""
     if len(p) == 4:
-        p[0] = (p[1], p[2], p[3])
+        p[0] = ("EXPR_PRE", p[1], p[2], p[3])
 
 
 def p_factor(p):
     """factor : factor_prefix postfix_expr"""
-    p[0] = (p[1], p[2])
+    p[0] = ("FACTOR", p[1], p[2])
+
 
 def p_factor_prefix(p):
     """factor_prefix : factor_prefix postfix_expr mulop
     | empty"""
     if len(p) == 4:
-        p[0] = (p[1], p[2], p[3])
+        p[0] = ("FACTOR_PREFIX", p[1], p[2], p[3])
 
 
 def p_postfix_expr(p):
     """postfix_expr : primary
     | call_expr"""
-    p[0] = [1]
+    p[0] = ("POSTFIX_EPR", p[1])
 
 
 def p_call_expr(p):
     """call_expr : id LPAREN expr_list RPAREN"""
-    p[0] = (p[1], p[2], p[3], p[4])
+    p[0] = ("CALL_EXPR", p[1], p[2], p[3], p[4])
 
 
 def p_expr_list(p):
     """expr_list : expr expr_list_tail
     | empty"""
     if len(p) == 3:
-        p[0] = (p[1], p[2])
+        p[0] = ("EXPR_LIST", p[1], p[2])
 
 
 def p_expt_list_tail(p):
     """expr_list_tail : COMMA expr expr_list_tail
     | empty"""
     if len(p) == 4:
-        p[0] = (p[1], p[2], p[3])
+        p[0] = ("EXPR_LIST_TAIL", p[1], p[2], p[3])
 
 
 def p_primary(p):
@@ -287,57 +288,57 @@ def p_primary(p):
     # add p[0] for value
     # This is where we get variable values.
     if len(p) == 4:
-        p[0] = (p[1], p[2], p[3])
+        p[0] = ("PRI", p[1], p[2], p[3])
     else:
-        p[0] = p[1]
+        p[0] = ("PRI", p[1])
 
 
 # """addop : + | -"""
 def p_addop(p):
     """addop : PLUS
     | MINUS"""
-    p[0] = p[1]
+    p[0] = ("BinOP", p[1])
 
 
 # """mulop :  | /"""
 def p_mulop(p):
     """mulop : TIMES
     | DIVIDE"""
-    p[0] = p[1]
+    p[0] = ("BinOP", p[1])
 
 
 ## Complex Statements and Condition
 def p_if_stmt(p):
     """if_stmt : IF LPAREN cond RPAREN pgm_body_var_decl_aux decl stmt_list end_if else_part ENDIF"""
     # FIXME removed rules not-lits going to empty
-    p[0] = (p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[9], p[10])
+    p[0] = ("IF", p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[9], p[10])
 
 def p_else_part(p):
     """else_part : ELSE pgm_body_var_decl_aux else_scope decl stmt_list
     | empty"""
     if len(p) == 6:
         #FIXME removed rules not-lits going to empty
-        p[0] = (p[1], p[4], p[5])
+        p[0] = ("ELSE", p[1], p[4], p[5])
     if (p[1]):
         end_scope()
 
 
 def p_cond(p):
     """cond : expr compop expr"""
-    p[0] = (p[1], p[2], [3])
+    p[0] = ("COND", p[1], p[2], p[3])
     new_cond_scope()
 
 
 def p_compop(p):
     """compop : COMPOP"""
-    p[0] = p[1]
+    p[0] = ("COMP_OP", p[1])
 
 
 ## While statements
 def p_while_stmt(p):
     """while_stmt : WHILE LPAREN cond RPAREN pgm_body_var_decl_aux decl stmt_list ENDWHILE"""
     # FIXME removed rules not-lits going to empty
-    p[0] = (p[1], p[2], p[3], p[4], p[6], p[7], p[8])
+    p[0] = ("WHILE", p[1], p[2], p[3], p[4], p[6], p[7], p[8])
     end_scope()
 
 
